b94c6fd02de18df80845b56065f2553e58ef61f5
shin- <joffrey@dotcloud.com>
2013-10-09 18:33:12 +0200

    Timers are started in daemon mode so they don't prevent the app from exiting

--- stackbrew/lib/periodic.py
+++ stackbrew/lib/periodic.py
@@ -9,13 +9,17 @@ def init_task(fn, period, lockfile='/opt/stackbrew/brw.lock', logger=None):
     def periodic(logger):
         if logger is not None:
             logger.info('Periodic task started')
-        threading.Timer(period, periodic, [logger]).start()
+        t = threading.Timer(period, periodic, [logger])
+        t.daemon = True
+        t.start()
         fn()
     if os.path.exists(lockfile):
         raise RuntimeError('Lockfile already present.')
     open(lockfile, 'w').close()
     lockfiles.append(lockfile)
-    threading.Timer(0, periodic, [logger]).start()
+    t = threading.Timer(0, periodic, [logger])
+    t.daemon = True
+    t.start()
 
 
 def clear_lockfiles(lockfiles):
