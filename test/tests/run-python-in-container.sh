a56df5f988569cc3134896df963f54a81b345940
Tianon Gravi <admwiggin@gmail.com>
2015-01-21 15:48:34 -0700

    Update all our in-container scripts to run with workdir set to the inContainerPath

--- test/tests/run-python-in-container.sh
+++ test/tests/run-python-in-container.sh
@@ -6,7 +6,7 @@ dir="$(readlink -f "$(dirname "$BASH_SOURCE")")"
 inContainerPath="/tmp/python"
 cmd=( "$inContainerPath/container.py" )
 
-if ! ret="$(docker run --rm -v "$dir":"$inContainerPath":ro --entrypoint sh "$1" -ec '
+if ! ret="$(docker run --rm -v "$dir":"$inContainerPath":ro -w "$inContainerPath" --entrypoint sh "$1" -ec '
 	for c in python3 python pypy3 pypy; do
 		if command -v "$c" > /dev/null; then
 			exec "$c" "$@"
--- test/tests/run-ruby-in-container.sh
+++ test/tests/run-ruby-in-container.sh
@@ -6,7 +6,7 @@ dir="$(readlink -f "$(dirname "$BASH_SOURCE")")"
 inContainerPath="/tmp/ruby"
 cmd=( "$inContainerPath/container.rb" )
 
-if ! ret="$(docker run --rm -v "$dir":"$inContainerPath":ro --entrypoint ruby "$1" "${cmd[@]}")"; then
+if ! ret="$(docker run --rm -v "$dir":"$inContainerPath":ro -w "$inContainerPath" --entrypoint ruby "$1" "${cmd[@]}")"; then
 	echo >&2 "error: '"$(basename "$dir")"' failed! got $ret"
 	exit 1
 fi
