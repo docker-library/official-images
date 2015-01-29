768a530672dfdf3356f465d71c281fa0f66a5a01
Moghedrin <nolat301@gmail.com>
2015-01-29 16:56:24 -0700

    Change run.sh to soft link

--- test/tests/haskell-ghci/run.sh
+++ test/tests/haskell-ghci/run.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-set -e
-
-dir="$(readlink -f "$(dirname "$BASH_SOURCE")")"
-
-inContainerPath="/tmp/bash"
-cmd=( "$inContainerPath/container.sh" )
-
-docker run --rm -v "$dir":"$inContainerPath":ro -w "$inContainerPath" --entrypoint bash "$1" "${cmd[@]}"
+run-bash-in-container.sh
