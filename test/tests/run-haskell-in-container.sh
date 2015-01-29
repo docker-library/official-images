2ea7e94c5f76a96d65a2393ded1b292602c15006
Moghedrin <nolat301@gmail.com>
2015-01-29 16:31:16 -0700

    Fix extension for haskell script

--- test/tests/run-haskell-in-container.sh
+++ test/tests/run-haskell-in-container.sh
@@ -4,6 +4,6 @@ set -e
 dir="$(readlink -f "$(dirname "$BASH_SOURCE")")"
 
 inContainerPath="/tmp/haskell"
-cmd=( "$inContainerPath/container.rb" )
+cmd=( "$inContainerPath/container.hs" )
 
 docker run --rm -v "$dir":"$inContainerPath":ro -w "$inContainerPath" --entrypoint runhaskell "$1" "${cmd[@]}"
