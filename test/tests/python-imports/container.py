c426e88c0db0112b753dccda8fe3561d888c856f
Deni Bertovic <deni@denibertovic.com>
2015-04-03 21:48:45 +0200

    rename to python-imports and add bz2
    Closes 560

--- test/tests/python-imports/container.py
+++ test/tests/python-imports/container.py
@@ -1,3 +1,6 @@
 import zlib
+import bz2
 
 assert(zlib.decompress(zlib.compress(b'IT WORKS IT WORKS IT WORKS')) == b'IT WORKS IT WORKS IT WORKS')
+assert(bz2.decompress(bz2.compress(b'IT WORKS IT WORKS IT WORKS')) == b'IT WORKS IT WORKS IT WORKS')
+
