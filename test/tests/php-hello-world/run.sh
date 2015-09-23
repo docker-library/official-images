40236c1433e73a0cf02d505825ba6153684b77e7
André R. <andre.romcke@gmail.com>
2015-09-23 08:48:58 +0200

    Update php-hello-world to remove -t param

--- test/tests/php-hello-world/run.sh
+++ test/tests/php-hello-world/run.sh
@@ -4,4 +4,4 @@ image="$1"
 dirTest="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 dirContainer='/var/www/html/hello-world'
 
-docker run -it --rm -v "$dirTest":"$dirContainer" -w "$dirContainer" "$image" php index.php
+docker run -i --rm -v "$dirTest":"$dirContainer" -w "$dirContainer" "$image" php index.php
