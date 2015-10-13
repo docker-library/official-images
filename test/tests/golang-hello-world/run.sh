84c2d895427ccbb2166a8d8fced1d122666151cd
Marcela Bonell <marcelabonell@gmail.com>
2015-10-13 17:18:55 -0500

    Closes #594: Use go run instead of go build

--- test/tests/golang-hello-world/run.sh
+++ test/tests/golang-hello-world/run.sh
@@ -5,4 +5,4 @@ image="$1"
 dirTest="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 dirContainer='/usr/src/hello-world'
 
-docker run --rm -v "$dirTest":"$dirContainer":ro -w "$dirContainer" "$image" sh -c 'go build -o /hello-world hello-world.go && /hello-world'
+docker run --rm -v "$dirTest":"$dirContainer":ro -w "$dirContainer" "$image" sh -c 'go run hello-world.go'
