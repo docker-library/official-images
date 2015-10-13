eeb9ebb57b165673ffff4b5df337a7b6e367198d
Marcela Bonell <marcelabonell@gmail.com>
2015-10-13 12:10:55 -0500

    Closes #591: Run the compiled file inside the container

--- test/tests/gcc-c-hello-world/run.sh
+++ test/tests/gcc-c-hello-world/run.sh
@@ -5,6 +5,4 @@ image="$1"
 dirTest="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 dirContainer='/usr/src/c'
 
-docker run --rm -v "$dirTest":"$dirContainer":rw -w "$dirContainer" "$image" gcc -o hello-world  hello-world.c
-"$dirTest"/hello-world
-rm -rf "$dirTest"/hello-world
+docker run --rm -v "$dirTest":"$dirContainer":ro -w "$dirContainer" "$image" sh -c 'gcc -o /hello-world hello-world.c && /hello-world'
