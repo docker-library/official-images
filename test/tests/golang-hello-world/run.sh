e9f5950c44c9c92109ee3d96d8465ebc91580da3
Marcela Bonell <marcelabonell@gmail.com>
2015-10-13 13:32:32 -0500

    Closes #594: Add set -e in the run script

--- test/tests/golang-hello-world/run.sh
+++ test/tests/golang-hello-world/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+set -e
 
 image="$1"
 dirTest="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 dirContainer='/usr/src/hello-world'
 
 docker run --rm -v "$dirTest":"$dirContainer":ro -w "$dirContainer" "$image" sh -c 'go build -o /hello-world hello-world.go && /hello-world'
-
