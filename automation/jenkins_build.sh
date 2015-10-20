b3489d6712b4560023413d38a06881e963b7274d
James <james@resin.io>
2015-10-20 18:56:37 +0700

    Update automation build script - Print failed images - The build will continue if an image fails but exit code 1 will be   returned

--- automation/jenkins_build.sh
+++ automation/jenkins_build.sh
@@ -10,6 +10,20 @@ push options:
 EOUSAGE
 }
 
+# If the build fails, set 1 as exit code and store failed image.
+is_success() {
+	if [ $1 -ne 0 ]; then
+		exitCode=1
+		if [ -z "$TAGS" ]; then
+			failedList+=($LIBRARY)
+		else
+			failedList+=($LIBRARY:$2)
+		fi
+	fi
+}
+
+exitCode=0
+failedList=()
 pushOnly=
 
 # Args handling
@@ -34,16 +48,26 @@ if [ -z "$TAGS" ]; then
 	if [ -z "$pushOnly" ]; then
 		# Build all images
 		./bashbrew.sh build $LIBRARY --library=../library --namespaces=resin
+		is_success $?
 	fi
 	# Push all images
 	./bashbrew.sh push $LIBRARY --library=../library --namespaces=resin
+	is_success $?
 else
 	for tag in $TAGS; do
 		if [ -z "$pushOnly" ]; then
 			# Build specified images only
 			./bashbrew.sh build $LIBRARY:$tag --library=../library --namespaces=resin
+			is_success $? $tag
 		fi
 		# Push specified images
 		./bashbrew.sh push $LIBRARY:$tag --library=../library --namespaces=resin
+		is_success $? $tag
 	done
+fi
+
+# if the build is marked as failed, print failed images
+if [ $exitCode -eq 1 ]; then
+	echo Failed images: ${failedList[@]}
+	exit 1
 fi	
