96a402b28ede9183c51fc13e0de0a00e66effefe
Tianon Gravi <admwiggin@gmail.com>
2015-11-09 15:43:33 -0800

    Switch ruby-hello-world test to use the common "run-ruby-in-container.sh" helper

--- test/tests/ruby-hello-world/run.sh
+++ test/tests/ruby-hello-world/run.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-set -e
-
-image="$1"
-dirTest="$(dirname "$(readlink -f "$BASH_SOURCE")")"
-dirContainer='/usr/src/ruby'
-
-docker run --rm -v "$dirTest":"$dirContainer":ro -w "$dirContainer" "$image" ruby hello-world.rb
+../run-ruby-in-container.sh
