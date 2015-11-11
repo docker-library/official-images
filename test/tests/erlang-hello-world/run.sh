894f72f1844d80aa4d318239ab154bd1f549ac67
Tianon Gravi <admwiggin@gmail.com>
2015-11-11 09:24:09 -0800

    Convert erlang-hello-world to use run-in-container.sh

--- test/tests/erlang-hello-world/run.sh
+++ test/tests/erlang-hello-world/run.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-set -e
-
-image="$1"
-dirTest="$(dirname "$(readlink -f "$BASH_SOURCE")")"
-dirContainer='/usr/src/erlang'
-
-docker run --rm -v "$dirTest":"$dirContainer":ro -w "$dirContainer" "$image" escript hello-world.erl
+../run-erlang-in-container.sh
