835f306263756a8d4ade5a41b5f492a96242337c
Tianon Gravi <admwiggin@gmail.com>
2015-12-04 15:13:57 -0800

    Switch from using "[[:space:]]" to just using "[ \t]" (especially so that mawk is an acceptable awk implementation)

--- test/tests/docker-build.sh
+++ test/tests/docker-build.sh
@@ -23,14 +23,14 @@ trap "rm -rf '$tmp'" EXIT
 
 cat > "$tmp/Dockerfile"
 
-from="$(awk -F '[[:space:]]+' 'toupper($1) == "FROM" { print $2; exit }' "$tmp/Dockerfile")"
+from="$(awk -F '[ \t]+' 'toupper($1) == "FROM" { print $2; exit }' "$tmp/Dockerfile")"
 onbuilds="$(docker inspect -f '{{len .Config.OnBuild}}' "$from")"
 if [ "$onbuilds" -gt 0 ]; then
 	# crap, the image we want to build has some ONBUILD instructions
 	# those are kind of going to ruin our day
 	# let's do some hacks to strip those bad boys out in a new fake layer
 	"$(dirname "$(readlink -f "$BASH_SOURCE")")/remove-onbuild.sh" "$from" "$imageTag"
-	awk -F '[[:space:]]+' 'toupper($1) == "FROM" { $2 = "'"$imageTag"'" } { print }' "$tmp/Dockerfile" > "$tmp/Dockerfile.new"
+	awk -F '[ \t]+' 'toupper($1) == "FROM" { $2 = "'"$imageTag"'" } { print }' "$tmp/Dockerfile" > "$tmp/Dockerfile.new"
 	mv "$tmp/Dockerfile.new" "$tmp/Dockerfile"
 fi
 
