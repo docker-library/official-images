330641bef9b9d8b7258b1d68fe91470e66e900a8
Tianon Gravi <admwiggin@gmail.com>
2016-02-23 13:22:35 -0800

    Add simple test for MySQL --log-bin

---
+++ test/tests/mysql-log-bin/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eo pipefail
+
+dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+image="$1"
+
+cname="mysql-container-$RANDOM-$RANDOM"
+cid="$(
+	docker run -d \
+		-e MYSQL_ALLOW_EMPTY_PASSWORD=1 \
+		--name "$cname" \
+		"$image" \
+		--log-bin="foo-$RANDOM" \
+		--server-id="$RANDOM"
+)"
+trap "docker rm -vf $cid > /dev/null" EXIT
+
+mysql() {
+	docker run --rm -i \
+		--link "$cname":mysql \
+		--entrypoint mysql \
+		"$image" \
+		-hmysql \
+		--silent \
+		"$@"
+}
+
+. "$dir/../../retry.sh" --tries 20 "echo 'SELECT 1' | mysql"
+
+# yay, must be OK
