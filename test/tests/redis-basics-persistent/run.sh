476e371c12a6b4d6958052a5f8fc50f6d7bfd5b8
Tim Düsterhus <tim@bastelstu.be>
2016-08-27 03:53:26 +0200

    Improve redis-basics-persistent by actually checking
    On SIGTERM a dump.rdb should be created when no special
    parameters are passed.
    
    see docker-library/redis#4

--- test/tests/redis-basics-persistent/run.sh
+++ test/tests/redis-basics-persistent/run.sh
@@ -5,10 +5,29 @@ dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 image="$1"
 
-newImage="$("$dir/../image-name.sh" librarytest/redis-basics-persistent "$image")"
-"$dir/../docker-build.sh" "$dir" "$newImage" <<EOD
-FROM $image
-CMD ["--appendonly", "yes"]
-EOD
+cname="redis-container-$RANDOM-$RANDOM"
+cid="$(docker run -d --name "$cname" "$image")"
+trap "docker rm -vf $cid > /dev/null" EXIT
 
-exec "$dir/real-run.sh" "$newImage"
+redis-cli() {
+	docker run --rm -i \
+		--link "$cname":redis \
+		--entrypoint redis-cli \
+		"$image" \
+		-h redis \
+		"$@"
+}
+
+# http://redis.io/topics/quickstart#check-if-redis-is-working
+
+. "$dir/../../retry.sh" --tries 20 '[ "$(redis-cli ping)" = "PONG" ]'
+
+[ "$(redis-cli set mykey somevalue)" = 'OK' ]
+[ "$(redis-cli get mykey)" = 'somevalue' ]
+
+docker stop "$cname"
+docker start "$cname"
+
+. "$dir/../../retry.sh" --tries 20 '[ "$(redis-cli ping)" = "PONG" ]'
+
+[ "$(redis-cli get mykey)" = 'somevalue' ]
