ddb1c2e0eb50e0dc4ca4be92e8b29994bcd98a9d
Alin Voinea <alin.voinea@gmail.com>
2016-10-06 23:41:54 +0300

    Make Add-ons environment variable more user friendly
    * Rename BUILDOUT_EGGS -> PLONE_ADDONS, ADDONS
    * Rename BUILDOUT_ZCML -> PLONE_ZCML, ZCML
    * Rename BUILDOUT_DEVELOP -> PLONE_DEVELOP, DEVELOP
    * Add Deprecation Warnings for renamed env variables

--- test/tests/plone-addons/run.sh
+++ test/tests/plone-addons/run.sh
@@ -7,7 +7,7 @@ PLONE_TEST_SLEEP=3
 PLONE_TEST_TRIES=5
 
 cname="plone-container-$RANDOM-$RANDOM"
-cid="$(docker run -d -e BUILDOUT_DEVELOP=src/eea.facetednavigation -e BUILDOUT_EGGS=eea.facetednavigation -e BUILDOUT_ZCML=eea.facetednavigation-meta --name "$cname" "$image" cat)"
+cid="$(docker run -d -e PLONE_DEVELOP=src/eea.facetednavigation -e PLONE_ADDONS=eea.facetednavigation -e PLONE_ZCML=eea.facetednavigation-meta --name "$cname" "$image" cat)"
 trap "docker rm -vf $cid > /dev/null" EXIT
 
 docker exec -it "$cname" cat custom.cfg
