dce1897e4741e2df8811663b955fdd622771fbf6
Alin Voinea <alin.voinea@gmail.com>
2016-10-08 15:16:13 +0300

    Fix issues reported by @tianon

--- test/tests/plone-basics/run.sh
+++ test/tests/plone-basics/run.sh
@@ -13,26 +13,26 @@ cid="$(docker run -d --name "$cname" "$image")"
 trap "docker rm -vf $cid > /dev/null" EXIT
 
 get() {
-  docker run --rm -i \
-             --link "$cname":plone \
-             --entrypoint python \
-                "$image" \
-                -c "import urllib2; con = urllib2.urlopen('$1'); print con.read()"
+	docker run --rm -i \
+		--link "$cname":plone \
+		--entrypoint python \
+		"$image" \
+		-c "import urllib2; con = urllib2.urlopen('$1'); print con.read()"
 }
 
 get_auth() {
-  docker run --rm -i \
-             --link "$cname":plone \
-             --entrypoint python \
-                "$image" \
-                -c "import urllib2; request = urllib2.Request('$1'); request.add_header('Authorization', 'Basic $2'); print urllib2.urlopen(request).read()"
+	docker run --rm -i \
+		--link "$cname":plone \
+		--entrypoint python \
+		"$image" \
+		-c "import urllib2; request = urllib2.Request('$1'); request.add_header('Authorization', 'Basic $2'); print urllib2.urlopen(request).read()"
 }
 
 
 . "$dir/../../retry.sh" --tries "$PLONE_TEST_TRIES" --sleep "$PLONE_TEST_SLEEP" get "http://plone:8080"
 
 # Plone is up and running
-[[ $(get 'http://plone:8080') == *"Plone is up and running"* ]]
+[[ "$(get 'http://plone:8080')" == *"Plone is up and running"* ]]
 
 # Create a Plone site
-[[ $(get_auth 'http://plone:8080/@@plone-addsite' 'YWRtaW46YWRtaW4=') == *"Create a Plone site"* ]]
+[[ "$(get_auth 'http://plone:8080/@@plone-addsite' "$(echo -n 'admin:admin' | base64)")" == *"Create a Plone site"* ]]
--- test/tests/plone-zeoclient/run.sh
+++ test/tests/plone-zeoclient/run.sh
@@ -20,25 +20,25 @@ pid="$(docker run -d --name "$pname" --link=$zname:zeo -e ZEO_ADDRESS=zeo:8100 "
 trap "docker rm -vf $pid $zid > /dev/null" EXIT
 
 get() {
-  docker run --rm -i \
-             --link "$pname":plone \
-             --entrypoint python \
-                "$image" \
-                -c "import urllib2; con = urllib2.urlopen('$1'); print con.read()"
+	docker run --rm -i \
+		--link "$pname":plone \
+		--entrypoint python \
+		"$image" \
+		-c "import urllib2; con = urllib2.urlopen('$1'); print con.read()"
 }
 
 get_auth() {
-  docker run --rm -i \
-             --link "$pname":plone \
-             --entrypoint python \
-                "$image" \
-                -c "import urllib2; request = urllib2.Request('$1'); request.add_header('Authorization', 'Basic $2'); print urllib2.urlopen(request).read()"
+	docker run --rm -i \
+		--link "$pname":plone \
+		--entrypoint python \
+		"$image" \
+		-c "import urllib2; request = urllib2.Request('$1'); request.add_header('Authorization', 'Basic $2'); print urllib2.urlopen(request).read()"
 }
 
 . "$dir/../../retry.sh" --tries "$PLONE_TEST_TRIES" --sleep "$PLONE_TEST_SLEEP" get "http://plone:8080"
 
 # Plone is up and running
-[[ $(get 'http://plone:8080') == *"Plone is up and running"* ]]
+[[ "$(get 'http://plone:8080')" == *"Plone is up and running"* ]]
 
 # Create a Plone site
-[[ $(get_auth 'http://plone:8080/@@plone-addsite' 'YWRtaW46YWRtaW4=') == *"Create a Plone site"* ]]
+[[ "$(get_auth 'http://plone:8080/@@plone-addsite' "$(echo -n 'admin:admin' | base64)")" == *"Create a Plone site"* ]]
