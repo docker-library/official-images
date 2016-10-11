278d4f0bc06e9c1f1b7622d09982652163638c74
Alin Voinea <alin.voinea@gmail.com>
2016-10-12 00:14:04 +0300

    Cleanup plone-addons test

--- test/tests/plone-addons/run.sh
+++ test/tests/plone-addons/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
-image="$1"
-
-cname="plone-container-$RANDOM-$RANDOM"
-cid="$(docker run -d -e PLONE_DEVELOP=src/eea.facetednavigation -e PLONE_ADDONS=eea.facetednavigation -e PLONE_ZCML=eea.facetednavigation-meta --name "$cname" "$image" cat)"
-trap "docker rm -vf $cid > /dev/null" EXIT
-
-docker exec "$cname" cat custom.cfg
+docker run -i --rm \
+	-e PLONE_DEVELOP=src/eea.facetednavigation \
+	-e PLONE_ADDONS=eea.facetednavigation \
+	-e PLONE_ZCML=eea.facetednavigation-meta \
+	-v /plone/instance/bin/develop \
+	"$1" cat custom.cfg
