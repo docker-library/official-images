31d60e0b4635159aa50a5f1c4966c09ab9fb75cd
Tianon Gravi <admwiggin@gmail.com>
2016-12-21 16:52:27 -0800

    Strip namespace when testing image name in "golang-hello-world"

--- test/tests/golang-hello-world/run.sh
+++ test/tests/golang-hello-world/run.sh
@@ -5,7 +5,7 @@ dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 image="$1"
 
-if [[ "$image" == gcc:4* ]]; then
+if [[ "${image##*/}" == gcc:4* ]]; then
 	echo >&2 'warning: gcc 4.x does not support Go'
 	cat "$dir/expected-std-out.txt" # cheaters gunna chea
 	exit
