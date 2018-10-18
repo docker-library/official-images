2fd881ae314cb8374ab8b08043c54da46afb5083
Andy Li <andy@onthewings.net>
2018-10-18 09:44:01 +0800

    haxe-haxelib-install: Remove `haxelib newrepo`, which is not available in Haxe 3.1 and it's not necessary anyway.

--- test/tests/haxe-haxelib-install/Container.hx
+++ test/tests/haxe-haxelib-install/Container.hx
@@ -1,9 +1,5 @@
 class Container {
     static function main():Void {
-        switch (Sys.command("haxelib", ["newrepo"])) {
-            case 0: //pass
-            case code: Sys.exit(code);
-        }
         switch (Sys.command("haxelib", ["install", "jQueryExtern"])) {
             case 0: //pass
             case code: Sys.exit(code);
