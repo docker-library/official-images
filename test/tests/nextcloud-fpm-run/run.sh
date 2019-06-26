fcae6a04f9794634666cfb03a19f9058f9756d31
Joe Ferguson <joe@infosiftr.com>
2019-06-26 15:09:23 -0700

    Adjust nextcloud-fpm and wordpress-fpm tests to use cgi-fcgi instead of nginx
    add retry to php-fpm test to fix "racy" test

--- test/tests/nextcloud-fpm-run/run.sh
+++ test/tests/nextcloud-fpm-run/run.sh
@@ -3,44 +3,45 @@ set -eo pipefail
 
 dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-# since we have curl in the php image, we'll use that
-clientImage="$1"
-serverImage="$1"
-
-# Create an instance of the container-under-test
-cid="$(docker run -d "$serverImage")"
-trap "docker rm -vf $cid > /dev/null" EXIT
+image="$1"
 
 # Build a client image with cgi-fcgi for testing
-nginxImage="$("$dir/../image-name.sh" librarytest/nextcloud-fpm-run-nginx "$1")"
-"$dir/../docker-build.sh" "$dir" "$nginxImage" <<EOD
-FROM nginx:alpine
-COPY dir/nginx-default.conf /etc/nginx/conf.d/default.conf
-EOD
+clientImage='librarytest/nextcloud-fpm-run:fcgi-client'
+docker build -t "$clientImage" - > /dev/null <<'EOF'
+FROM debian:stretch-slim
 
-serverImage="$1"
+RUN set -x && apt-get update && apt-get install -y libfcgi0ldbl && rm -rf /var/lib/apt/lists/*
+
+ENTRYPOINT ["cgi-fcgi"]
+EOF
 
 # Create an instance of the container-under-test
-cid="$(docker run -d "$serverImage")"
+cid="$(docker run -d "$image")"
 trap "docker rm -vf $cid > /dev/null" EXIT
-nginxCid="$(docker run -d --link "$cid":fpm --volumes-from "$cid" "$nginxImage")"
-trap "docker rm -vf $nginxCid $cid > /dev/null" EXIT
 
-_request() {
+fcgi-request() {
 	local method="$1"
-	shift
 
-	local url="${1#/}"
-	shift
+	local url="$2"
+	local queryString=
+	if [[ "$url" == *\?* ]]; then
+		queryString="${url#*\?}"
+		url="${url%%\?*}"
+	fi
 
-	docker run --rm --link "$nginxCid":nginx "$clientImage" \
-		curl -fsL -X"$method" "$@" "http://nginx/$url"
+	docker run --rm -i --link "$cid":fpm \
+		-e REQUEST_METHOD="$method" \
+		-e SCRIPT_NAME="$url" \
+		-e SCRIPT_FILENAME=/var/www/html/"${url#/}" \
+		-e QUERY_STRING="$queryString" \
+		"$clientImage" \
+		-bind -connect fpm:9000
 }
 
 # Make sure that PHP-FPM is listening and ready
-. "$dir/../../retry.sh" --tries 30 '_request GET / --output /dev/null'
+. "$dir/../../retry.sh" --tries 30 'fcgi-request GET /index.php' > /dev/null 2>&1
 
 # Check that we can request / and that it contains the pattern "Finish setup" somewhere
 # <input type="submit" class="primary" value="Finish setup" data-finishing="Finishing …">
-_request GET '/' |tac|tac| grep -iq "Finish setup"
+fcgi-request GET '/index.php' |tac|tac| grep -iq "Finish setup"
 # (without "|tac|tac|" we get "broken pipe" since "grep" closes the pipe before "curl" is done reading it)
