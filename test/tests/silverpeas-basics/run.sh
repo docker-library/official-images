18e0b61153b7a0605b2b15adf16cace4e11904ab
Joe Ferguson <joe@infosiftr.com>
2020-06-19 14:53:50 -0700

    Adjust silverpeas-basics test to ensure remote postgresql connectivity
    - also use generated names over container ids

--- test/tests/silverpeas-basics/run.sh
+++ test/tests/silverpeas-basics/run.sh
@@ -12,45 +12,57 @@ if ! docker image inspect "$clientImage" &> /dev/null; then
 	docker pull "$clientImage" > /dev/null
 fi
 
+pgImage='postgres:12.3'
+if ! docker image inspect "$pgImage" &> /dev/null; then
+	docker pull "$pgImage" > /dev/null
+fi
+
+dbuser='postgres'
+dbpass="silver-$RANDOM-$RANDOM"
+dbdatabase='silverpeas'
+
 # run a PostgresSQL container
 dbname="postgresql-container-$RANDOM-$RANDOM"
-dbid="$(
-  docker run -d \
-    --name "$dbname" \
-    -e POSTGRES_PASSWORD=silverpeas \
-    postgres:12.3
-)"
-trap "docker rm -vf $dbid > /dev/null" EXIT
+docker run -d \
+	--name "$dbname" \
+	-e POSTGRES_PASSWORD=$dbpass \
+	-e POSTGRES_DB=$dbdatabase \
+	"$pgImage"
+trap "docker rm -vf $dbname > /dev/null" EXIT
 
 check_db_running() {
-	docker exec "$dbid" psql -U postgres -d 'postgres' -c 'SELECT 1'
+	docker run \
+		--rm \
+		--link "$dbname":pg \
+		-e PGPASSWORD="$dbpass" \
+		"$pgImage" \
+		psql \
+		--host pg \
+		--username "$dbuser" \
+		--dbname "$dbdatabase" \
+		-c 'SELECT 1'
 }
 
-# wait for PostgreSQL to be ran
+# wait for PostgreSQL to be ready outside of container localhost
 . "$dir/../../retry.sh" --tries 20 --sleep 5 'check_db_running'
 
-# create the database for testing Silverpeas
-docker exec "$dbid" psql -U postgres -c 'create database silverpeas' > /dev/null
-
-cname="silverpeas-container-$RANDOM-$RANDOM"
 # when running the first time, a silverpeas process is spawn before starting Silverpeas
 # (this configuration process can take some time)
-cid="$(
-  docker run -d \
-    --name "$cname" \
-    --link "$dbid":database \
-    -e DB_SERVERTYPE=POSTGRESQL \
-    -e DB_NAME=silverpeas \
-    -e DB_SERVER=database \
-    -e DB_USER=postgres \
-    -e DB_PASSWORD=silverpeas \
-    "$image"
-)"
-trap "docker rm -vf $cid $dbid > /dev/null" EXIT
+cname="silverpeas-container-$RANDOM-$RANDOM"
+docker run -d \
+	--name "$cname" \
+	--link "$dbname":pg \
+	-e DB_SERVER=pg \
+	-e DB_SERVERTYPE=POSTGRESQL \
+	-e DB_NAME="$dbdatabase" \
+	-e DB_USER="$dbuser" \
+	-e DB_PASSWORD="$dbpass" \
+	"$image"
+trap "docker rm -vf $cname $dbname > /dev/null" EXIT
 
 check_running() {
 	docker run --rm \
-		--link "$cid":silverpeas \
+		--link "$cname":silverpeas \
 		"$clientImage" \
 		curl -fs http://silverpeas:8000/silverpeas > /dev/null
 }
