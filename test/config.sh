578d7edfc24fa83e02c1c083a8b27897b7eaad0b
Fernandez Ludovic <lfernandez.dev@gmail.com>
2020-07-10 02:39:34 +0200

    Bump Traefik to v2.2.3

--- test/config.sh
+++ test/config.sh
@@ -308,6 +308,9 @@ globalExcludeTests+=(
 	[nats-streaming:nanoserver_override-cmd]=1
 	[nats-streaming:windowsservercore_override-cmd]=1
 
+	# https://github.com/docker-library/official-images/pull/8329#issuecomment-656383836
+	[traefik:windowsservercore_override-cmd]=1
+
 	# TODO adjust MongoDB tests to use docker networks instead of links so they can work on Windows (and consider using PowerShell to generate appropriate certificates for TLS tests instead of openssl)
 	[mongo:windowsservercore_mongo-basics]=1
 	[mongo:windowsservercore_mongo-auth-basics]=1
