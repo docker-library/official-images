0d33d9a56dc2865333f29ec78bac4ed749949342
J0WI <J0WI@users.noreply.github.com>
2020-11-08 21:26:36 +0100

    Add tests for phpMyAdmin

--- test/config.sh
+++ test/config.sh
@@ -182,6 +182,12 @@ imageTests+=(
 	[php:fpm]='
 		php-fpm-hello-web
 	'
+	[phpmyadmin:apache]='
+		phpmyadmin-apache-run
+	'
+	[phpmyadmin:fpm]='
+		phpmyadmin-fpm-run
+	'
 	[plone]='
 		plone-basics
 		plone-addons
