b1f807085e6410abfed95f280b3732479acc1942
Alexis Saettler <alexis@saettler.org>
2021-06-23 08:09:01 +0200

    Update Monica to v3.1.0

--- test/tests/monica-cli-mysql8/run.sh
+++ test/tests/monica-cli-mysql8/run.sh
@@ -5,7 +5,7 @@ dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 image="$1"
 
-dbImage='mysql:5.7'
+dbImage='mysql:8.0'
 # ensure the dbImage is ready and available
 if ! docker image inspect "$dbImage" &> /dev/null; then
 	docker pull "$dbImage" > /dev/null
