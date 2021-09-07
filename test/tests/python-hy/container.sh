a553f496cc37a546ffd8eb87bf45fe73460cf22b
Tianon Gravi <admwiggin@gmail.com>
2021-09-07 16:41:10 -0700

    Update python-hy test to 1.0a3
    Also, now that Python 3.10 is "frozen" (https://pythoninsider.blogspot.com/2021/09/python-3100rc2-is-available.html), allow it to run the tests on Linux.

--- test/tests/python-hy/container.sh
+++ test/tests/python-hy/container.sh
@@ -16,8 +16,8 @@ fi
 
 # Hy is complicated, and uses Python's internal AST representation directly, and thus Hy releases usually lag behind a little on major Python releases (and we don't want that to gum up our tests)
 # see https://github.com/hylang/hy/issues/1111 for example breakage
-if ! "$python" -c 'import sys; exit((sys.version_info[0] == 3 and sys.version_info[1] >= 10) or sys.version_info[0] > 3 or sys.version_info[0] == 2)'; then
-	echo >&2 'skipping Hy test -- not allowed on Python 2.x and 3.10+ (yet!)'
+if ! "$python" -c 'import sys; exit((sys.version_info[0] == 3 and sys.version_info[1] >= 11) or sys.version_info[0] > 3 or sys.version_info[0] == 2)'; then
+	echo >&2 'skipping Hy test -- not allowed on Python 2.x and 3.11+ (yet!)'
 	# cheaters gunna cheat
 	cat expected-std-out.txt
 	exit
@@ -27,6 +27,7 @@ fi
 export PIP_DISABLE_PIP_VERSION_CHECK=1
 # or that a new version will no longer work with this python version
 export PIP_NO_PYTHON_VERSION_WARNING=1
-pip install -q 'hy==0.20.0'
+# https://pypi.org/project/hy/#history
+pip install -q 'hy==1.0a3'
 
 hy ./container.hy
