aced34c5a627ada09c0b0ab32a95d3b38ca91f1d
J0WI <J0WI@users.noreply.github.com>
2022-12-01 21:10:12 +0100

    Alpine 3.17

--- test/tests/rabbitmq-tls/run.sh
+++ test/tests/rabbitmq-tls/run.sh
@@ -30,12 +30,12 @@ EOD
 
 testImage="$("$dir/../image-name.sh" librarytest/rabbitmq-tls-test "$1")"
 "$dir/../docker-build.sh" "$dir" "$testImage" <<'EOD'
-FROM alpine:3.14
+FROM alpine:3.17
 RUN apk add --no-cache bash coreutils drill openssl procps
 # https://github.com/drwetter/testssl.sh/releases
-ENV TESTSSL_VERSION 3.0.5
+ENV TESTSSL_VERSION 3.0.8
 RUN set -eux; \
-	wget -O testssl.tgz "https://github.com/drwetter/testssl.sh/archive/${TESTSSL_VERSION}.tar.gz"; \
+	wget -O testssl.tgz "https://github.com/drwetter/testssl.sh/archive/v${TESTSSL_VERSION}.tar.gz"; \
 	tar -xvf testssl.tgz -C /opt; \
 	rm testssl.tgz; \
 	ln -sv "/opt/testssl.sh-$TESTSSL_VERSION/testssl.sh" /usr/local/bin/; \
