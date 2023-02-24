9d7fb5103b8846f9b5f7f1757c2b795f1f2544d5
David Matějček <david.matejcek@omnifish.ee>
2023-02-24 17:50:39 +0100

    Test extended by the stop command too + updated images
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- test/tests/glassfish/run.sh
+++ test/tests/glassfish/run.sh
@@ -5,17 +5,23 @@ serverImage="$1"
 containerId="$(docker run -d "$serverImage")"
 trap "docker rm -vf $containerId > /dev/null" EXIT
 
-logLine='^\s+Eclipse GlassFish\s+[\.0-9]+'
-timeout=60
+waitForLogLine() {
+    timeout="$1";
+    logLine="$2";
+    until docker logs $containerId 2>&1 | grep -q -E "$logLine"
+    do
+        if [ $timeout -eq 0 ]
+        then
+            exit 100;
+        fi
+        sleep 1
+        timeout=$((timeout-1))
+    done
+}
 
-until docker logs $containerId 2>&1 | grep -q -E "$logLine"
-do
-    if [ $timeout -eq 0 ]
-    then
-        exit 100;
-    fi
-    sleep 1
-    timeout=$((timeout-1))
-done
+waitForLogLine 60 '^\s+Eclipse GlassFish\s+[\.0-9]+';
+echo "GlassFish started as ${containerId}"
 
-echo "Success!"
+docker stop "${containerId}" &
+waitForLogLine 30 '^\s+Completed shutdown of GlassFish runtime';
+echo "GlassFish stopped OK!"
